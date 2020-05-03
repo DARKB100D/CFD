9cc57aef2f39e3bce52d8c1519b8cf910d45333b
DARKB100D <MATRIX.1.3@yandex.ru>
2020-05-03 11:40:24 +0300

    Fix StartupWindow. Add slots.

--- src/gui/StartupWindow.cpp
+++ src/gui/StartupWindow.cpp
@@ -1,1 +1,54 @@
 #include "StartupWindow.h"
+
+// This is included here because it is forward declared in
+// StartupWindow.h
+#include "ui_StartupWindow.h"
+
+#include "..\gui\mainWindow.h"
+
+StartupWindow::StartupWindow() {
+	// Load UI
+	this->ui = new Ui_StartupWindow;
+	this->ui->setupUi(this);
+
+	// connect signals and slots
+	connectObjects();
+
+	// fill last projects list
+	fillProjectHistory();
+}
+
+void StartupWindow::btnCreateProject_Click()
+{
+	this->hide();
+	MainWindow * window = new MainWindow();
+	window->show();
+}
+
+void StartupWindow::btnOpenProject_Click()
+{
+
+}
+
+void StartupWindow::projectList_Click()
+{
+
+}
+
+void StartupWindow::connectObjects()
+{
+	connect(ui->btnCreateProject, SIGNAL(clicked()), this, SLOT(btnCreateProject_Click()));
+	connect(ui->btnOpenProject, SIGNAL(clicked()), this, SLOT(btnOpenProject_Click()));
+	connect(ui->ProjectsListWidget, SIGNAL(doubleclicked()), this, SLOT(ProjectList_Click()));
+}
+
+void StartupWindow::fillProjectHistory()
+{
+	QListWidgetItem * item = new QListWidgetItem("Test project (D:\\Hemodynamics\\CFD\\testproject)");
+	ui->ProjectsListWidget->addItem(item);
+}
+
+void StartupWindow::openProject()
+{
+}
+
--- src/gui/StartupWindow.h
+++ src/gui/StartupWindow.h
@@ -2,8 +2,25 @@
 
 #include <QMainWindow>
 
+// Forward Qt class declarations
+class Ui_StartupWindow;
+
 class StartupWindow : public QMainWindow
 {
+public:
+	StartupWindow();
+	~StartupWindow() = default;
 
+private:
+	Q_OBJECT
+	Ui_StartupWindow * ui;
+	void connectObjects();
+	void fillProjectHistory();
+	void openProject();
+
+public slots:
+	void btnCreateProject_Click();
+	void btnOpenProject_Click();
+	void projectList_Click();
 };
 
