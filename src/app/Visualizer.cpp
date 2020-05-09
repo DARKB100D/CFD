703a7ded240a7318a9187e75e74b557f448c7f58
DARKB100D <MATRIX.1.3@yandex.ru>
2020-05-09 21:18:57 +0300

    Rename "Visualizator" to "Visualizer"

--- src/app/Visualizer.cpp
+++ src/app/Visualizer.cpp
@@ -1,4 +1,4 @@
-#include "Visualizator.h"
+#include "Visualizer.h"
 
 #include "ui_MainWindow.h"
 
@@ -11,7 +11,7 @@
 #include <vtkWindowToImageFilter.h>
 #include <vtkPNGWriter.h>
 
-Visualizator::Visualizator(Ui_MainWindow * ui)
+Visualizer::Visualizer(Ui_MainWindow * ui)
 {
 	// VTK Renderer
 	renderer = vtkSmartPointer<vtkRenderer>::New();
@@ -27,14 +27,14 @@ Visualizator::Visualizator(Ui_MainWindow * ui)
 	ui->qvtkWidget->GetRenderWindow()->AddRenderer(renderer);
 }
 
-Visualizator::~Visualizator()
+Visualizer::~Visualizer()
 {
 }
 
 
-void Visualizator::visualizeModel(std::string inputFilename) {
+void Visualizer::loadModel(QString inputFilename) {
 	vtkSmartPointer<vtkSTLReader> reader = vtkSmartPointer<vtkSTLReader>::New();
-	reader->SetFileName(inputFilename.c_str());
+	reader->SetFileName(inputFilename.toLocal8Bit());
 	reader->Update();
 
 	// Visualize
@@ -49,20 +49,38 @@ void Visualizator::visualizeModel(std::string inputFilename) {
 	this->normalizeSize();
 }
 
-void Visualizator::visualizeMesh() {
+void Visualizer::loadMesh(QString inputFilename) {
 
 }
 
-void Visualizator::visualizeResult() {
+void Visualizer::loadResult(QString inputFilename) {
 
 }
 
-void Visualizator::normalizeSize() {
+void Visualizer::setShowModel(bool _show)
+{
+	showModel = _show;
+	this->update();
+}
+
+void Visualizer::setShowMesh(bool _show)
+{
+	showMesh = _show;
+	this->update();
+}
+
+void Visualizer::setShowResult(bool _show)
+{
+	showResult = _show;
+	this->update();
+}
+
+void Visualizer::normalizeSize() {
 	this->renderer->ResetCamera();
 	this->renderer->GetRenderWindow()->Render();
 }
 
-void Visualizator::saveScreenshot(std::string selectedFilename) {
+void Visualizer::saveScreenshot(QString selectedFilename) {
 	vtkSmartPointer<vtkWindowToImageFilter> windowToImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
 	windowToImageFilter->SetInput(this->renderer->GetRenderWindow());
 	windowToImageFilter->SetScale(2); // image quality
@@ -71,9 +89,16 @@ void Visualizator::saveScreenshot(std::string selectedFilename) {
 	windowToImageFilter->Update();
 
 	vtkSmartPointer<vtkPNGWriter> writer = vtkSmartPointer<vtkPNGWriter>::New();
-	writer->SetFileName(selectedFilename.c_str());
+	writer->SetFileName(selectedFilename.toLocal8Bit());
 	writer->SetInputConnection(windowToImageFilter->GetOutputPort());
 	writer->Write();
 
 	this->renderer->GetRenderWindow()->Render(); // normalize
+}
+
+void Visualizer::update()
+{
+	if (showModel) {}
+	if (showMesh) {}
+	if (showResult) {}
 }
---
+++ src/app/Visualizer.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "IVisualize.h"
+#include <vtkSmartPointer.h>
+#include <vtkActor.h>
+
+class Ui_MainWindow;
+
+class Visualizer : public IVisualize
+{
+public:
+	Visualizer(Ui_MainWindow * ui);
+	~Visualizer();
+	void loadModel(QString inputFilename);
+	void loadMesh(QString inputFilename);
+	void loadResult(QString inputFilename);
+	void setShowModel(bool _show);
+	void setShowMesh(bool _show);
+	void setShowResult(bool _show);
+	void normalizeSize();
+	void saveScreenshot(QString selectedFilename);
+
+private:
+	vtkSmartPointer<vtkRenderer> renderer;
+	bool showModel;
+	bool showMesh;
+	bool showResult;
+	void update();
+
+};
+
